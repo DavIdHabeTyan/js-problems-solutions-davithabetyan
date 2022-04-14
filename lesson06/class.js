6cee5fa9c8b719cdc8f61daa4ec3a4bc3e4c1825
David Habetyan <davidhabetyanm@gmail.com>
2022-04-14 20:09:10 +0400

    added homework 1

--- lesson06/class.js
+++ lesson06/class.js
@@ -1,3 +1,116 @@
+//homework 1  Create an Author class and a Book class.
+// Author should have: name, email, gende
+describe('classes', () => {
+
+  describe('1. ', () => {
+    class Author {
+      #name;
+      #email;
+      #gender;
+
+      constructor(name, email, gender) {
+        this.#name = name;
+        this.#email = email;
+        this.#gender = gender;
+      }
+
+
+
+      set name(name) {
+          if (name === '') {
+              throw new Error(`name field of Author cannot be empty`);
+          }
+          this.#name = name;
+      }
+fit
+      get email() {
+        return this.#email;
+      }
+
+      set email(email) {
+          if (email === '') {
+              throw new Error(`email field of Author cannot be empty`);
+          }
+          this.#email = email;
+      }
+
+
+      set gender(gender) {
+          if (gender === '') {
+              throw new Error(`gender field of Author cannot be empty`);
+          }
+          this.#gender = gender;
+      }
+
+      toString() {
+        return 'author'
+      }
+    }
+
+    class Book {
+      #title;
+      #author;
+      #price;
+      #quantity;
+
+
+      get title() {
+        return this.#title;
+      }
+
+      set title(title) {
+        if (title === '') {
+          throw new Error(`title field of Book cannot be empty`);
+        }
+        this.#title = title;
+      }
+
+
+      get author() {
+        return this.#author;
+      }
+
+      set author(author) {
+        if (author === '') {
+          throw new Error(`author field of Book cannot be empty`);
+        }
+        this.#author = author;
+      }
+
+
+      get price() {
+        return this.#price;
+      }
+
+      set price(price) {
+        if (price === '') {
+          throw new Error(`price field of Book cannot be empty`);
+        }
+        this.#price = price;
+      }
+
+
+      get quantity() {
+        return this.#quantity;
+      }
+
+      set quantity(quantity) {
+        if (quantity === '') {
+          throw new Error(`quantity field of Book cannot be empty`);
+        }
+        this.#quantity = quantity;
+      }
+
+    }
+
+    test('1. ', () => {
+
+    })
+  });
+
+
+//homework 2 Create an Account class. Account should have: id, name, balance. It
+// should have setters for name and balance, and getters for all fields.
 class Account {
   #id;
   #name;
